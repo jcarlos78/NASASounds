38a6c298086f66a99d43184f0154392cc50536e3
José Carlos Menezes Pereira <jcarlos78@gmail.com>
2014-11-15 17:49:03 -0200

    Share and play buttons.

--- app/src/main/java/tipmobil/com/br/nasasounds/MyActivity.java
+++ app/src/main/java/tipmobil/com/br/nasasounds/MyActivity.java
@@ -1,35 +1,34 @@
 package tipmobil.com.br.nasasounds;
 
 import android.app.Activity;
-import android.content.Context;
-import android.content.res.AssetFileDescriptor;
+import android.content.Intent;
 import android.content.res.Resources;
-import android.hardware.Sensor;
-import android.hardware.SensorManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AbsListView;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.ListAdapter;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.lang.reflect.Field;
-import java.util.List;
 
 
 public class MyActivity extends Activity {
 
     ListView listView ;
     ArrayAdapter<Item> adapter;
+    MediaPlayer mp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,22 +51,60 @@ public class MyActivity extends Activity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> l, View view, int position, long id) {
-                Item sound = (Item)listView.getItemAtPosition(position);
-                Log.d("############", "Items " + sound.arquivo);
-                MediaPlayer mp = getSound(sound.arquivo);
-                if(mp!=null) {
-                    mp.start();
-                }
+                playSound(position);
             }
         });
+
+    }
+
+    public void playSound(int position){
+        if(mp!=null) {
+            mp.stop();
+        }
+        Item sound = (Item)listView.getItemAtPosition(position);
+        mp = getSound(sound.arquivo);
+        if(mp!=null) {
+            mp.start();
+        }
+    }
+
+    public void shareSound(int position){
+        //Item sound = (Item)listView.getItemAtPosition(position);
+        //Intent share = new Intent(Intent.ACTION_SEND);
+        //share.setType("audio/*");
+        //share.putExtra(Intent.EXTRA_STREAM, getShare(sound.arquivo));
+        //startActivity(Intent.createChooser(share, "Share Sound File"));
+
+        Item sound = (Item)listView.getItemAtPosition(position);
+        File dest = Environment.getExternalStorageDirectory();
+        InputStream in = getResources().openRawResource(getShare(sound.arquivo));
+
+        try
+        {
+            OutputStream out = new FileOutputStream(new File(dest, sound.arquivo+".mp3"));
+            byte[] buf = new byte[1024];
+            int len;
+            while ( (len = in.read(buf, 0, buf.length)) != -1)
+            {
+                out.write(buf, 0, len);
+            }
+            in.close();
+            out.close();
+        }
+        catch (Exception e) {}
+
+        Intent share = new Intent(Intent.ACTION_SEND);
+        share.setType("audio/*");
+        share.putExtra(Intent.EXTRA_STREAM, Uri.parse(Environment.getExternalStorageDirectory().toString() + "/"+sound.arquivo+".mp3"));
+        startActivity(Intent.createChooser(share, "Share Sound File"));
 
     }
 
     private MediaPlayer getSound(String soundName){
         if(soundName.equals("eagle_has_landed")){
             return MediaPlayer.create(this, R.raw.eagle_has_landed);
-        }else if(soundName.equals("computers_are_in_control")){
-            return MediaPlayer.create(this, R.raw.computers_are_in_control);
+        }else if(soundName.equals("computers_in_control")){
+            return MediaPlayer.create(this, R.raw.computers_in_control);
         }else if(soundName.equals("go_for_deploy")){
             return MediaPlayer.create(this, R.raw.go_for_deploy);
         }else if(soundName.equals("houston_problem")){
@@ -90,6 +127,35 @@ public class MyActivity extends Activity {
             return MediaPlayer.create(this, R.raw.sputnik_beep);
         }
         return null;
+    }
+
+    private int getShare(String soundName){
+        if(soundName.equals("eagle_has_landed")){
+            return R.raw.eagle_has_landed;
+        }else if(soundName.equals("computers_in_control")){
+            return R.raw.computers_in_control;
+        }else if(soundName.equals("go_for_deploy")){
+            return R.raw.go_for_deploy;
+        }else if(soundName.equals("houston_problem")){
+            return R.raw.houston_problem;
+        }else if(soundName.equals("kennedy_we_choose")){
+            return R.raw.kennedy_we_choose;
+        }else if(soundName.equals("landing_gear_drop")){
+            return R.raw.landing_gear_drop;
+        }else if(soundName.equals("launch")){
+            return R.raw.launch;
+        }else if(soundName.equals("launch_nats")){
+            return R.raw.launch_nats;
+        }else if(soundName.equals("liftoff")){
+            return R.raw.liftoff;
+        }else if(soundName.equals("saturn_radio_waves")){
+            return R.raw.saturn_radio_waves;
+        }else if(soundName.equals("small_step")){
+            return R.raw.small_step;
+        }else if(soundName.equals("sputnik_beep")){
+            return R.raw.sputnik_beep;
+        }
+        return 0;
     }
 
     private Item[] getRawFiles(){
